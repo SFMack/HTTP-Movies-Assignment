ea1e0db1d35d5c832703d465e76353c94f0d3eb8
SFMack <sfcamack@icloud.com>
2020-05-01 22:06:50 -0700

    minor edit

--- client/src/Movies/MovieForm.js
+++ client/src/Movies/MovieForm.js
@@ -7,16 +7,17 @@ function MovieForm({ updateMovie }) {
     const [director, setDirector] = useState('');
     const [metascore, setMetascore] = useState('');
     const [stars, setStars] = useState([]);
-    const [movie, setMovie] = useState({});
+    const [movie, setMovie] = useState({
+    });
 
     const sendUpdatedMovie = (e) => {
         e.preventDefault();
-        updateMovie(movie)
+        setMovie(title, director, metascore, stars)
+        console.log(movie);
+        // updateMovie(movie)
     }
 
-    // useEffect(() => {
-        
-    // })
+    // useEffect(() => {})
 
     console.log(title);
     return (
@@ -24,10 +25,31 @@ function MovieForm({ updateMovie }) {
             <form>
                 <input 
                     type="text"
-                    name="id"
+                    name="title"
                     value={title}
                     onChange={(e) => setTitle(e.target.value)}
                 />
+                <input 
+                    type="text"
+                    name="director"
+                    value={director}
+                    onChange={(e) => setDirector(e.target.value)}
+                />
+                <input 
+                    type="text"
+                    name="metascore"
+                    value={metascore}
+                    onChange={(e) => setMetascore(e.target.value)}
+                />
+                <input 
+                    type="text"
+                    name="stars"
+                    value={stars}
+                    onChange={(e) => setStars(e.target.value.split(","))}
+                />
+                <button onClick={sendUpdatedMovie}>
+                    Update
+                </button>
             </form>
         </div>
     )
